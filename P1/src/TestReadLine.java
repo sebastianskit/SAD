aa5641aee533a1ff5b455d7af6a6e57a2051affd
rupi8 <arnau.ruiz.pique@estudiantat.upc.edu>
2024-10-03 16:35:53 +0200

    BuffereReader closed correctly

--- P1/src/TestReadLine.java
+++ P1/src/TestReadLine.java
@@ -11,14 +11,16 @@ public class TestReadLine { // Clase principal para probar la funcionalidad de l
         line.addObserver(console); // Añade la consola como observadora de la línea.
 
         BufferedReader reader = new EditableBufferedReader(console, new InputStreamReader(System.in), line); // Crea el
-                                                                                                             // lector
-        // editable.
+        // lector editable.
 
         try {
             String result = reader.readLine(); // Lee la línea editada por el usuario.
             System.out.println("\nLinea editada: " + result); // Muestra el resultado final después de que el usuario
             // presione Enter.
             System.out.println();
+            reader.close(); // Cerramos recursos de reader, ya que si no tenemos el warning debido a la
+                            // necesidad de BuffereReader
+            // se cierre correctamente
         } catch (IOException e) {
             e.printStackTrace(); // Muestra un error si ocurre algún problema durante la lectura.
         }
