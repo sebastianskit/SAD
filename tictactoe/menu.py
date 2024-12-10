aa4fca166257679fce066e02c5848647fff35809
USERSAD\aruipiqu <aruipiqu@emeal.nttdata.com>
2024-12-10 15:20:27 +0100

    adaptation theoric MVC

--- tictactoe/menu.py
+++ tictactoe/menu.py
@@ -1,3 +1,23 @@
-import tablero
-
-
+"""
+MVC
+    -> model
+        -> constants
+            COLORS black, white, red, green, blue
+            DIMENSIONS width, height, extra_size, height2.....
+        -> Variables
+        -> States
+        -> Screen setup
+        -> Sounds
+        -> Display texts
+        -> Display texts coordinates
+    -> vista
+        -> drawInitScreen()
+        -> drawGameScreen()
+        -> drawEndScreen()
+    -> controller
+        -> drawFigures()
+        -> checkWinner()
+        -> isDraw();
+        -> gameReset()
+        
+"""
