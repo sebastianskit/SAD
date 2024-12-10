33afab1ba1259e05f7b092aea6794ad880c84b99
USERSAD\aruipiqu <aruipiqu@emeal.nttdata.com>
2024-12-10 15:52:14 +0100

    update

--- tictactoe/menu.py
+++ tictactoe/menu.py
@@ -1,23 +1,31 @@
 """
 MVC
-    -> model
-        -> constants
-            COLORS black, white, red, green, blue
-            DIMENSIONS width, height, extra_size, height2.....
-        -> Variables
-        -> States
-        -> Screen setup
-        -> Sounds
-        -> Display texts
-        -> Display texts coordinates
-    -> vista
-        -> drawInitScreen()
-        -> drawGameScreen()
-        -> drawEndScreen()
-    -> controller
-        -> drawFigures()
-        -> checkWinner()
-        -> isDraw();
-        -> gameReset()
-        
+
+tictactoe/
+├── controllers/
+│   ├── game_controller.py
+            -> update()
+            -> events()     
+├── models/
+│   ├── game_state.py
+            -> gameReset()
+            -> checkWinner()
+            -> isDraw()
+│   ├── constants.py              
+├── views/
+│   ├── game_view.py
+             -> drawInitScreen()
+             -> drawGameScreen()
+             -> drawEndScreen()
+             -> drawFigures()          
+│   ├── assets/
+│       ├── sounds/
+│           ├── click.mp3
+│           ├── click2.mp3
+│           ├── victory.mp3
+│           ├── draw.mp3
+│           ├── error.mp3
+│       ├── images/            
+├── main.py                    
+
 """
